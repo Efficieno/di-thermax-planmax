a2575c80de66ad8528c270c59de159bbff3c6dfa
Demo User <demo@user.com>
2024-11-13 15:47:41 +0000

    "Created file di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_header_dtls.py"

--- di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_header_dtls.py
+++ di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_header_dtls.py
@@ -7,6 +7,7 @@ from efficieno.components.erd_objects import ERDBase, ColumnMetadata
 
 if TYPE_CHECKING:
     from ..ont.oe_order_lines_all import OeOrderLinesAll
+    from ..ont.oe_order_lines_all import OeOrderLinesAll
     from ..ont.oe_order_headers_all import OeOrderHeadersAll
     from ..xxtmx_planmax.xxplanmax_line_dtls import XxplanmaxLineDtls
     from ..xxtmx_planmax.xxplanmax_line_dtls import XxplanmaxLineDtls
@@ -159,6 +160,11 @@ class XxplanmaxHeaderDtls(ERDBase):
     sos_item_id: Mapped[str] = mapped_column('sos_item_id', Numeric, primary_key=False, info={"column_metadata": ColumnMetadata()})
     reflection_completion_date: Mapped[str] = mapped_column('reflection_completion_date', DateTime, primary_key=False, info={"column_metadata": ColumnMetadata()})
 
+        
+
+    
+
+    OeOrderLinesAll_header_id: Mapped["OeOrderLinesAll"] = relationship(back_populates="XxplanmaxHeaderDtls_sales_order_header_id", primaryjoin="OeOrderLinesAll.header_id==XxplanmaxHeaderDtls.sales_order_header_id", foreign_keys="[OeOrderLinesAll.header_id]", viewonly=True)
         
 
     
