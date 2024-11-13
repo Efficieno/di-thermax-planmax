9b039e403a2e1b2e274bb689b5ad2a59c88178a6
Demo User <demo@user.com>
2024-11-13 15:54:28 +0000

    "Created file di_thermax_planmax/erd/apps/org_organization_definitions.py"

--- di_thermax_planmax/erd/apps/org_organization_definitions.py
+++ di_thermax_planmax/erd/apps/org_organization_definitions.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from efficieno.components.erd_objects import ERDBase, ColumnMetadata
 
 if TYPE_CHECKING:
+    from ..xxtmx_planmax.xxplanmax_header_dtls import XxplanmaxHeaderDtls
     from ..apps.mtl_reservations_v import MtlReservationsV
     from ..apps.mtl_onhand_total_v import MtlOnhandTotalV
     from ..ont.oe_order_headers_all import OeOrderHeadersAll
@@ -33,33 +34,46 @@ class OrgOrganizationDefinitions(ERDBase):
     operating_unit: Mapped[str] = mapped_column('operating_unit', Integer, primary_key=False, info={"column_metadata": ColumnMetadata()})
     legal_entity: Mapped[str] = mapped_column('legal_entity', Integer, primary_key=False, info={"column_metadata": ColumnMetadata()})
 
-    MtlReservationsV_organization_id: Mapped["MtlReservationsV"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id",
-                                                                                primaryjoin="MtlReservationsV.organization_id==OrgOrganizationDefinitions.organization_id",
-                                                                                foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+        
 
-    MtlOnhandTotalV_organization_id: Mapped["MtlOnhandTotalV"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id",
-                                                                              primaryjoin="MtlOnhandTotalV.organization_id==OrgOrganizationDefinitions.organization_id",
-                                                                              foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+    
 
-    OeOrderHeadersAll_ship_from_org_id: Mapped["OeOrderHeadersAll"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id",
-                                                                                   primaryjoin="OeOrderHeadersAll.ship_from_org_id==OrgOrganizationDefinitions.organization_id",
-                                                                                   foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+    XxplanmaxHeaderDtls_mfg_organization_id: Mapped["XxplanmaxHeaderDtls"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="XxplanmaxHeaderDtls.mfg_organization_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+        
 
-    OeOrderLinesAll_ship_from_org_id: Mapped["OeOrderLinesAll"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id",
-                                                                               primaryjoin="OeOrderLinesAll.ship_from_org_id==OrgOrganizationDefinitions.organization_id",
-                                                                               foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+    
 
-    WipDiscreteJobs_organization_id: Mapped["WipDiscreteJobs"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id",
-                                                                              primaryjoin="WipDiscreteJobs.organization_id==OrgOrganizationDefinitions.organization_id",
-                                                                              foreign_keys="[WipDiscreteJobs.organization_id]", viewonly=True)
+    MtlReservationsV_organization_id: Mapped["MtlReservationsV"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="MtlReservationsV.organization_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+        
 
-    HrOperatingUnits_organization_id: Mapped["HrOperatingUnits"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id",
-                                                                                primaryjoin="HrOperatingUnits.organization_id==OrgOrganizationDefinitions.organization_id",
-                                                                                foreign_keys="[HrOperatingUnits.organization_id]", viewonly=True)
+    
 
-    MtlSystemItemsB_organization_id: Mapped["MtlSystemItemsB"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id",
-                                                                              primaryjoin="MtlSystemItemsB.organization_id==OrgOrganizationDefinitions.organization_id",
-                                                                              foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+    MtlOnhandTotalV_organization_id: Mapped["MtlOnhandTotalV"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="MtlOnhandTotalV.organization_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+        
+
+    
+
+    OeOrderHeadersAll_ship_from_org_id: Mapped["OeOrderHeadersAll"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="OeOrderHeadersAll.ship_from_org_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+        
+
+    
+
+    OeOrderLinesAll_ship_from_org_id: Mapped["OeOrderLinesAll"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="OeOrderLinesAll.ship_from_org_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
+        
+
+    
+
+    WipDiscreteJobs_organization_id: Mapped["WipDiscreteJobs"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="WipDiscreteJobs.organization_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[WipDiscreteJobs.organization_id]", viewonly=True)
+        
+
+    
+
+    HrOperatingUnits_organization_id: Mapped["HrOperatingUnits"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="HrOperatingUnits.organization_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[HrOperatingUnits.organization_id]", viewonly=True)
+        
+
+    
+
+    MtlSystemItemsB_organization_id: Mapped["MtlSystemItemsB"] = relationship(back_populates="OrgOrganizationDefinitions_organization_id", primaryjoin="MtlSystemItemsB.organization_id==OrgOrganizationDefinitions.organization_id", foreign_keys="[OrgOrganizationDefinitions.organization_id]", viewonly=True)
 
 
 
