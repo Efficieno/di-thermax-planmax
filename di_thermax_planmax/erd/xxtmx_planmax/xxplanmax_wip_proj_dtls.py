8bd5829e4b4b8169328d36e14b195c437a3a94a4
Demo User <demo@user.com>
2024-11-13 15:20:14 +0000

    "Created file di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_wip_proj_dtls.py"

--- di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_wip_proj_dtls.py
+++ di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_wip_proj_dtls.py
@@ -11,7 +11,7 @@ class XxplanmaxWipProjDtls(ERDBase):
     __data_object_type__ = "data_object"
     __tablename__ = "xxplanmax_wip_proj_dtls"
     __table_args__ = {"schema": "xxtmx_planmax", "extend_existing": True}
-    __table_properties__ = {"ui_x_pos": 0, "ui_y_pos": 0, "colour": "#F2F3F5"}
+    __table_properties__ = {"ui_x_pos": -737.5357666015625, "ui_y_pos": 914.0802612304688, "colour": "#F2F3F5"}
 
     organization_code: Mapped[str] = mapped_column('organization_code', String, primary_key=True, info={"column_metadata": ColumnMetadata()})
     organization_id: Mapped[str] = mapped_column('organization_id', Numeric, primary_key=False, info={"column_metadata": ColumnMetadata()})
