6dfc85de644ec157a4a032b998637c6c9a04270e
Demo User <demo@user.com>
2024-11-13 15:07:38 +0000

    "Created file di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_model_xref.py"

---
+++ di_thermax_planmax/erd/xxtmx_planmax/xxplanmax_model_xref.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Date, Float, Integer, String, Numeric, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from efficieno.components.erd_objects import ERDBase, ColumnMetadata
+
+
+
+class XxplanmaxModelXref(ERDBase):
+    __data_object_type__ = "data_object"
+    __tablename__ = "xxplanmax_model_xref"
+    __table_args__ = {"schema": "xxtmx_planmax", "extend_existing": True}
+    __table_properties__ = {"ui_x_pos": 0, "ui_y_pos": 0, "colour": "#F2F3F5"}
+
+    model_item: Mapped[str] = mapped_column('model_item', String, primary_key=True, info={"column_metadata": ColumnMetadata()})
+    model_number: Mapped[str] = mapped_column('model_number', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    product_planner: Mapped[str] = mapped_column('product_planner', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    std_rsmh: Mapped[str] = mapped_column('std_rsmh', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    prn_applicable: Mapped[str] = mapped_column('prn_applicable', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    full_lead_time: Mapped[str] = mapped_column('full_lead_time', Numeric, primary_key=False, info={"column_metadata": ColumnMetadata()})
+    product_category: Mapped[str] = mapped_column('product_category', String, primary_key=False, info={"column_metadata": ColumnMetadata()})
+
+
+
+
