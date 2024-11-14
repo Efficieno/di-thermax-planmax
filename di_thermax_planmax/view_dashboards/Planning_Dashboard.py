556829a177dbd008d5eafa83a16a8c6d32459d7c
Demo User <demo@user.com>
2024-11-14 09:30:18 +0000

    "Updated Dashboard"

--- di_thermax_planmax/view_dashboards/Planning_Dashboard.py
+++ di_thermax_planmax/view_dashboards/Planning_Dashboard.py
@@ -1,20 +1,18 @@
 from efficieno.components.dashboard_objects import PanelComponent, Dashboard, ColumnProperty
 from di_thermax_planmax.ontologies.planmax_headers import PlanmaxHeaders
+from di_thermax_planmax.ontologies.planmax_headers import PlanmaxHeaders
+from di_thermax_planmax.ontologies.planmax_headers import PlanmaxHeaders
 
 
 class PlanningDashboard(Dashboard):
     __dashboard_name__ = "Planning Dashboard"
     __dashboard_description__ = "Planning Dashboard"
 
-    __grid_root_element__ = {'type': 'branch', 
-                             'data': [
-                                 {'type': 'leaf', 'data': {'views': ['charts_orders_by_status'], 'activeView': 'charts_orders_by_status', 'id': '1'}, 'size': 1778.296875},
-                                 {'type': 'leaf', 'data': {'views': ['charts_orders_by_group'], 'activeView': 'charts_orders_by_group', 'id': '1'}, 'size': 1778.296875}
-                             ], 'size': 707.53125}
-    __grid_width__ = 1778.296875
-    __grid_height__ = 707.53125
-    __grid_orientation__ = "HORIZONTAL"
-    __active_group__ = 1
+    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['charts_orders_by_status'], 'activeView': 'charts_orders_by_status', 'id': '1'}, 'size': 763}, {'type': 'leaf', 'data': {'views': ['charts_orders_by_group'], 'activeView': 'charts_orders_by_group', 'id': '2'}, 'size': 763.578125}], 'size': 244}, {'type': 'leaf', 'data': {'views': ['metrics_JZSFQImkw'], 'activeView': 'metrics_JZSFQImkw', 'id': '3'}, 'size': 244.3125}], 'size': 1526.578125}
+    __grid_width__ = 1526.578125
+    __grid_height__ = 488.3125
+    __grid_orientation__ = "VERTICAL"
+    __active_group__ = 3
 
     charts_orders_by_status = PanelComponent(component_type="charts",
                           name="Orders by Status",
@@ -23,59 +21,10 @@ class PlanningDashboard(Dashboard):
                           header="Open Orders by Status",
                           description="Open Orders by Status",
                           columns=[],
-                          chart_options={
-    "title": {
-        "text": "Order Status: Count and Value"
-    },
-    "tooltip": {
-        "trigger": "axis"
-    },
-    "legend": {
-        "data": ["Count", "Value"]
-    },
-    "xAxis": {
-        "type": "category",
-        "axisLabel": {
-            "rotate": 45
-        }
-    },
-    "yAxis": [
-        {
-            "type": "value",
-            "name": "Count",
-            "position": "left"
-        },
-        {
-            "type": "value",
-            "name": "Value",
-            "position": "right"
-        }
-    ],
-    "series": [
-        {
-            "type": "bar",
-            "encode": {
-                "x": "order_status",
-                "y": "count"
-            },
-            "yAxisIndex": 0,
-            "name": "Count"
-        },
-        {
-            "type": "bar",
-            "encode": {
-                "x": "order_status",
-                "y": "value"
-            },
-            "yAxisIndex": 1,
-            "name": "Value"
-        }
-    ]
-},
+                          chart_options={'legend': {'data': ['Count', 'Value']}, 'series': [{'encode': {'x': 'order_status', 'y': 'count'}, 'name': 'Count', 'type': 'bar', 'yAxisIndex': 0}, {'encode': {'x': 'order_status', 'y': 'value'}, 'name': 'Value', 'type': 'bar', 'yAxisIndex': 1}], 'title': {'text': 'Order Status: Count and Value'}, 'tooltip': {'trigger': 'axis'}, 'xAxis': {'axisLabel': {'rotate': 45}, 'type': 'category'}, 'yAxis': [{'name': 'Count', 'position': 'left', 'type': 'value'}, {'name': 'Value', 'position': 'right', 'type': 'value'}]},
                           content_component="mediator",
                           relations=[],
                           metadata={})
-
     charts_orders_by_group = PanelComponent(component_type="charts",
                           name="Orders by Group",
                           query="Select(PlanmaxHeaders.group_name, func.count(PlanmaxHeaders.sales_order_header_id).label('count'), func.sum(PlanmaxHeaders.total_unit_value_in_inr).label('value')).group_by(PlanmaxHeaders.group_name)",
@@ -83,56 +32,18 @@ class PlanningDashboard(Dashboard):
                           header="Orders by Group",
                           description="Orders by Group",
                           columns=[],
-                          chart_options={
-    "title": {
-        "text": "Order Status: Count and Value"
-    },
-    "tooltip": {
-        "trigger": "axis"
-    },
-    "legend": {
-        "data": ["Count", "Value"]
-    },
-    "xAxis": {
-        "type": "category",
-        "axisLabel": {
-            "rotate": 45
-        }
-    },
-    "yAxis": [
-        {
-            "type": "value",
-            "name": "Count",
-            "position": "left"
-        },
-        {
-            "type": "value",
-            "name": "Value",
-            "position": "right"
-        }
-    ],
-    "series": [
-        {
-            "type": "bar",
-            "encode": {
-                "x": "group_name",
-                "y": "count"
-            },
-            "yAxisIndex": 0,
-            "name": "Count"
-        },
-        {
-            "type": "bar",
-            "encode": {
-                "x": "group_name",
-                "y": "value"
-            },
-            "yAxisIndex": 1,
-            "name": "Value"
-        }
-    ]
-},
+                          chart_options={'legend': {'data': ['Count', 'Value']}, 'series': [{'encode': {'x': 'group_name', 'y': 'count'}, 'name': 'Count', 'type': 'bar', 'yAxisIndex': 0}, {'encode': {'x': 'group_name', 'y': 'value'}, 'name': 'Value', 'type': 'bar', 'yAxisIndex': 1}], 'title': {'text': 'Order Status: Count and Value'}, 'tooltip': {'trigger': 'axis'}, 'xAxis': {'axisLabel': {'rotate': 45}, 'type': 'category'}, 'yAxis': [{'name': 'Count', 'position': 'left', 'type': 'value'}, {'name': 'Value', 'position': 'right', 'type': 'value'}]},
                           content_component="mediator",
                           relations=[],
                           metadata={})
-
+    metrics_JZSFQImkw = PanelComponent(component_type="metrics",
+                          name="Demo Header",
+                          query="",
+                          data_objects={'PlanmaxHeaders': 'di_thermax_planmax.ontologies.planmax_headers'},
+                          header="Demo Header",
+                          description="Demo Description",
+                          columns=[],
+                          chart_options={},
+                          content_component="mediator",
+                          relations=[],
+                          metadata={'display': '1000', 'font_size': '2', 'font_style': ['bold'], 'additional_field_visible': True})
