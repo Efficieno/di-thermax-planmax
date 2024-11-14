d88fc1927b2c5a8286bd2c1c671641bdaec98402
Sai Kiran <neo2603@gmail.com>
2024-11-14 13:54:32 +0530

    Update Planning_Dashboard.py

--- di_thermax_planmax/view_dashboards/Planning_Dashboard.py
+++ di_thermax_planmax/view_dashboards/Planning_Dashboard.py
@@ -14,12 +14,60 @@ class PlanningDashboard(Dashboard):
 
     charts_booked_order_by_curr = PanelComponent(component_type="charts",
                           name="Booked Orders by Curr",
-                          query="Select(PlanmaxHeaders.order_status, func.count(PlanmaxHeaders.sales_order_header_id).label('count')).group_by(PlanmaxHeaders.order_status)",
+                          query="Select(PlanmaxHeaders.order_status, func.count(PlanmaxHeaders.sales_order_header_id).label('count'), func.sum(PlanmaxHeaders.total_unit_value_in_inr).label('value')).group_by(PlanmaxHeaders.order_status)",
                           data_objects={'PlanmaxHeaders': 'di_thermax_planmax.ontologies.planmax_headers'},
                           header="Open Orders by Status",
                           description="Open Orders by Status",
                           columns=[],
-                          chart_options={'series': [{'label': {'margin': 8, 'show': True}, 'name': 'Items', 'type': 'bar'}], 'title': [{'show': True, 'subtext': 'Open orders', 'text': 'Open Orders By Status'}], 'tooltip': {'show': True}, 'xAxis': {'type': 'category'}, 'yAxis': {}},
+                          chart_options={
+    "title": {
+        "text": "Order Status: Count and Value"
+    },
+    "tooltip": {
+        "trigger": "axis"
+    },
+    "legend": {
+        "data": ["Count", "Value"]
+    },
+    "xAxis": {
+        "type": "category",
+        "axisLabel": {
+            "rotate": 45
+        }
+    },
+    "yAxis": [
+        {
+            "type": "value",
+            "name": "Count",
+            "position": "left"
+        },
+        {
+            "type": "value",
+            "name": "Value",
+            "position": "right"
+        }
+    ],
+    "series": [
+        {
+            "type": "bar",
+            "encode": {
+                "x": "order_status",
+                "y": "count"
+            },
+            "yAxisIndex": 0,
+            "name": "Count"
+        },
+        {
+            "type": "bar",
+            "encode": {
+                "x": "order_status",
+                "y": "value"
+            },
+            "yAxisIndex": 1,
+            "name": "Value"
+        }
+    ]
+},
                           content_component="mediator",
                           relations=[],
                           metadata={})
