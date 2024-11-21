96fea2d97a6c6c5927d1fd25df657840eb7ba6bb
Demo User <demo@user.com>
2024-11-21 16:48:23 +0000

    "Updated Dashboard"

--- di_thermax_planmax/view_dashboards/planning.py
+++ di_thermax_planmax/view_dashboards/planning.py
@@ -12,11 +12,11 @@ class Planning(Dashboard):
     __dashboard_name__ = "planning"
     __dashboard_description__ = "Planning Dashboard"
 
-    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['charts__aEywZ_Sh'], 'activeView': 'charts__aEywZ_Sh', 'id': '1'}, 'size': 644}, {'type': 'leaf', 'data': {'views': ['charts_hL_B6ejVG'], 'activeView': 'charts_hL_B6ejVG', 'id': '2'}, 'size': 488}, {'type': 'leaf', 'data': {'views': ['charts_6oVivWQha'], 'activeView': 'charts_6oVivWQha', 'id': '5'}, 'size': 394.578125}], 'size': 232}, {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['tables_UOdbwrgzB'], 'activeView': 'tables_UOdbwrgzB', 'id': '3'}, 'size': 763}, {'type': 'leaf', 'data': {'views': ['tables_X21l_Bolp'], 'activeView': 'tables_X21l_Bolp', 'id': '4'}, 'size': 763.578125}], 'size': 232}, {'type': 'leaf', 'data': {'views': ['charts_JH9qfdbAn'], 'activeView': 'charts_JH9qfdbAn', 'id': '6'}, 'size': 233.375}], 'size': 1526.578125}
-    __grid_width__ = 1526.578125
+    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['charts__aEywZ_Sh'], 'activeView': 'charts__aEywZ_Sh', 'id': '1'}, 'size': 753}, {'type': 'leaf', 'data': {'views': ['charts_hL_B6ejVG'], 'activeView': 'charts_hL_B6ejVG', 'id': '2'}, 'size': 571}, {'type': 'leaf', 'data': {'views': ['charts_6oVivWQha'], 'activeView': 'charts_6oVivWQha', 'id': '5'}, 'size': 462.09375}], 'size': 348}, {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['tables_UOdbwrgzB', 'tables_X21l_Bolp'], 'activeView': 'tables_X21l_Bolp', 'id': '3'}, 'size': 1384}, {'type': 'leaf', 'data': {'views': ['charts_JH9qfdbAn'], 'activeView': 'charts_JH9qfdbAn', 'id': '6'}, 'size': 402.09375}], 'size': 349.375}], 'size': 1786.09375}
+    __grid_width__ = 1786.09375
     __grid_height__ = 697.375
     __grid_orientation__ = "VERTICAL"
-    __active_group__ = 6
+    __active_group__ = 3
 
     charts__aEywZ_Sh = PanelComponent(component_type="charts",
                           name="Order Info Chart",
