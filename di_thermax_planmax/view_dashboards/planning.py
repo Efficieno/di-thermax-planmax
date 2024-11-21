958603001aba48e9255c67ab3b542475212f974a
Demo User <demo@user.com>
2024-11-21 12:23:27 +0000

    "Updated Dashboard"

--- di_thermax_planmax/view_dashboards/planning.py
+++ di_thermax_planmax/view_dashboards/planning.py
@@ -1,16 +1,14 @@
 from efficieno.components.dashboard_objects import PanelComponent, Dashboard, ColumnProperty, FilterField, FilterComponent
 from sqlalchemy import Integer, Select, bindparam, String
 from di_thermax_planmax.ontologies.planmax_headers import PlanmaxHeaders
-from di_thermax_planmax.ontologies.order_headers import OrderHeaders
-from di_thermax_planmax.ontologies.order_headers import OrderHeaders
 
 
 class Planning(Dashboard):
     __dashboard_name__ = "planning"
     __dashboard_description__ = "Planning Dashboard"
 
-    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['charts__aEywZ_Sh'], 'activeView': 'charts__aEywZ_Sh', 'id': '1'}, 'size': 249}, {'type': 'leaf', 'data': {'views': ['panel__NXT8QrunU'], 'activeView': 'panel__NXT8QrunU', 'id': '2'}, 'size': 249}, {'type': 'leaf', 'data': {'views': ['panel_SrQ6YWB5ry'], 'activeView': 'panel_SrQ6YWB5ry', 'id': '3'}, 'size': 247.46875}], 'size': 1526.578125}
-    __grid_width__ = 1526.578125
+    __grid_root_element__ = {'type': 'branch', 'data': [{'type': 'leaf', 'data': {'views': ['charts__aEywZ_Sh'], 'activeView': 'charts__aEywZ_Sh', 'id': '1'}, 'size': 745.46875}], 'size': 1786.09375}
+    __grid_width__ = 1786.09375
     __grid_height__ = 745.46875
     __grid_orientation__ = "VERTICAL"
     __active_group__ = 1
@@ -23,97 +21,10 @@ class Planning(Dashboard):
                           description="Demo Description",
                           content_component="mediator",
                           relations=[],
-                          metadata={'chartOptions': {
-   "legend":{
-      "data":[
-         
-      ],
-      "left":"center",
-      "orient":"horizontal",
-      "show":True
-   },
-   "series":[
-      {
-        "datasetIndex": None,
-         "encode":{
-            "itemName":"",
-            "value":"",
-            "x":"order_status",
-            "y":"count"
-         },
-         "name":"",
-         "type":"bar"
-      },
-      {
-        "datasetIndex": None,
-         "encode":{
-            "itemName":"",
-            "value":"",
-            "x":"order_status",
-            "y":"value"
-         },
-         "name":"",
-         "type":"line"
-      }
-   ],
-   "title":{
-      "left":"left",
-      "subtext":"",
-      "text":"Order Information",
-      "top":"top"
-   },
-   "tooltip":{
-      "formatter":"",
-      "show":True,
-      "trigger":"item"
-   },
-   "xAxis":{
-      "name":"",
-      "show":True,
-      "type":"category"
-   },
-   "yAxis":[
-      {
-         "name":"count",
-         "position":"left",
-         "type":"value"
-      },
-      {
-         "name":"value",
-         "position":"right",
-         "type":"value"
-      }
-   ]
-}},
+                          metadata={'chartOptions': {'legend': {'data': [], 'left': 'center', 'orient': 'horizontal', 'show': True}, 'series': [{'datasetIndex': None, 'encode': {'itemName': 'month_name', 'value': '', 'x': 'month_name', 'y': 'count'}, 'name': '', 'type': 'bar'}, {'datasetIndex': None, 'encode': {'itemName': 'month_name', 'value': '', 'x': 'month_name', 'y': 'value'}, 'name': '', 'type': 'line'}], 'title': {'left': 'left', 'subtext': '', 'text': 'Order Information', 'top': 'top'}, 'tooltip': {'formatter': '', 'show': True, 'trigger': 'item'}, 'xAxis': {'name': '', 'show': True, 'type': 'category'}, 'yAxis': [{'name': 'count', 'position': 'left', 'type': 'value'}, {'name': 'value', 'position': 'right', 'type': 'value'}]}},
                           drill_downs={'0': {'name': 'Month', 'value': 'PlanmaxHeaders.month_name'}, '1': {'name': 'Group', 'value': 'PlanmaxHeaders.group_name'}, '2': {'name': 'Region', 'value': 'PlanmaxHeaders.region_of_order'}, '3': {'name': 'Product Category', 'value': 'PlanmaxHeaders.product_category'}, '4': {'name': 'MFG Organization', 'value': 'PlanmaxHeaders.mfg_organization_code'}, '5': {'name': 'Planner', 'value': 'PlanmaxHeaders.planner'}, '6': {'name': 'Regional Commercial', 'value': 'PlanmaxHeaders.regional_commercial'}},
                           actions=[],
                           inline_actions={'name': '', 'value': ''},
-                          details=[])
-    panel__NXT8QrunU = PanelComponent(component_type="metrics",
-                          name="Panel 1",
-                          query="Select(func.count(OrderHeaders.header_id).label('Count Orders'))",
-                          data_objects={'OrderHeaders': 'demo_project.ontologies.order_headers'},
-                          header="Demo Metric Header",
-                          description="Demo Metric Description",
-                          content_component="default",
-                          relations=[],
-                          metadata={},
-                          drill_downs={},
-                          actions=[],
-                          inline_actions=[],
-                          details=[])
-    panel_SrQ6YWB5ry = PanelComponent(component_type="metrics",
-                          name="Panel 1",
-                          query="Select(func.count(OrderHeaders.header_id).label('Count Orders'))",
-                          data_objects={'OrderHeaders': 'demo_project.ontologies.order_headers'},
-                          header="Demo Metric Header",
-                          description="Demo Metric Description",
-                          content_component="default",
-                          relations=[],
-                          metadata={},
-                          drill_downs={},
-                          actions=[],
-                          inline_actions=[],
                           details=[])
 
 
