01bd22c3db090da2663b447d811b57d5f155e151
Sai Kiran <neo2603@gmail.com>
2024-11-26 23:26:00 +0530

    Update drp_details.py

--- di_thermax_planmax/ontologies/drp_details.py
+++ di_thermax_planmax/ontologies/drp_details.py
@@ -1,1 +1,19 @@
-import os
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Date, Float, Integer, String, Numeric, DateTime, join
+from sqlalchemy.orm import Mapped, mapped_column, relationship, column_property
+from efficieno.components.erd_objects import ERDBase, ColumnMetadata
+
+from di_thermax_planmax.erd.xxtmx_planmax.xxplanmax_header_dtls import XxplanmaxHeaderDtls
+from di_thermax_planmax.erd.xxtmx_planmax.xxplanmax_cust_dtls import XxplanmaxCustDtls
+from di_thermax_planmax.erd.apps.org_organization_definitions import OrgOrganizationDefinitions
+from di_thermax_planmax.erd.xxtmx_planmax.xxplanmax_calender import XxplanmaxCalender
+from di_thermax_planmax.erd.xxtmx_planmax.xxplanmax_drp_details import XxplanmaxDrpDetails
+
+
+class PlanmaxHeaders(ERDBase):
+    # __table__ = (join(XxplanmaxHeaderDtls, XxplanmaxCustDtls, XxplanmaxHeaderDtls.XxplanmaxCustDtls_site_use_id.expression)
+    #              .join(OrgOrganizationDefinitions, XxplanmaxHeaderDtls.OrgOrganizationDefinitions_organization_id.expression))
+    # __table__ = XxplanmaxHeaderDtls.__table__
+    __table__ = join(XxplanmaxHeaderDtls, XxplanmaxCalender, XxplanmaxHeaderDtls.XxplanmaxCalender_day_id.expression)
+    __table_properties__ = {"ui_x_pos": 503.13734483912555, "ui_y_pos": 891.5731584714746, "colour": "#F2F3F5"}
