29932f99102b8c581ad12c77bf80c284daaa50ea
Sai Kiran <neo2603@gmail.com>
2024-11-29 12:57:33 +0530

    Update update_order_fields.py

--- di_thermax_planmax/view_actions/update_order_fields.py
+++ di_thermax_planmax/view_actions/update_order_fields.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Integer, Select, bindparam, String, Date, Update, and_, func
 from efficieno.components.actions_object import Action, Parameter
-
+from efficieno.data_sources.data_sources import DataSource
 from di_thermax_planmax.erd.apps.org_organization_definitions import OrgOrganizationDefinitions
 from di_thermax_planmax.erd.inv.mtl_system_items_b import MtlSystemItemsB
 from di_thermax_planmax.erd.xxtmx_planmax.xxplanmax_model_xref import XxplanmaxModelXref
@@ -189,5 +189,7 @@ class UpdateOrderFields(Action):
                       rated_standard_man_hrs=cls.rated_standard_man_hrs.current_value,
                       reason_for_otp=cls.reason_for_otp.current_value))
         print(f"Executing SQL Statement {update_sql}")
-        print("Completed Execution")
+        data_source = DataSource()
+        result = data_source.execute_update(update_sql)
+        print(f"Completed Execution {result}")
         return {}
