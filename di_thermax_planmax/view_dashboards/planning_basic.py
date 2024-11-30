a7ad20e81aeaf7b215eea0830e20d4b6e441ebaf
Sai Kiran <neo2603@gmail.com>
2024-11-30 15:39:30 +0530

    Update planning_basic.py

--- di_thermax_planmax/view_dashboards/planning_basic.py
+++ di_thermax_planmax/view_dashboards/planning_basic.py
@@ -165,7 +165,7 @@ class PlanningBasic(Dashboard):
                           drill_downs={},
                           actions=[],
                           inline_actions={'name': '', 'value': ''},
-                          details=[])
+                          details=["tables_vlndGn9kq"])
     metrics_PPKe1QH2J = PanelComponent(component_type="metrics",
                           name="Demo Header",
                           query="""Select(func.count(PlanmaxHeaders.sales_order_header_id).label('count')).filter(PlanmaxHeaders.reflection_config_status != 'C').filter(PlanmaxHeaders.order_status.not_in(['CLOSED', 'CANCELLED']))""",
