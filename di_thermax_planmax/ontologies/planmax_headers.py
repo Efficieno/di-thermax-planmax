67f05ca9423c2adff1de4e214df3ef67373137de
Sai Kiran <neo2603@gmail.com>
2024-12-02 10:06:57 +0530

    Update planmax_headers.py

--- di_thermax_planmax/ontologies/planmax_headers.py
+++ di_thermax_planmax/ontologies/planmax_headers.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Date, Float, Integer, String, Numeric, DateTime, join
+from sqlalchemy import Date, Float, Integer, String, Numeric, DateTime, join, outerjoin
 from sqlalchemy.orm import Mapped, mapped_column, relationship, column_property
 from efficieno.components.erd_objects import ERDBase, ColumnMetadata
 
@@ -14,7 +14,7 @@ class PlanmaxHeaders(ERDBase):
     # __table__ = (join(XxplanmaxHeaderDtls, XxplanmaxCustDtls, XxplanmaxHeaderDtls.XxplanmaxCustDtls_site_use_id.expression)
     #              .join(OrgOrganizationDefinitions, XxplanmaxHeaderDtls.OrgOrganizationDefinitions_organization_id.expression))
     # __table__ = XxplanmaxHeaderDtls.__table__
-    __table__ = join(XxplanmaxHeaderDtls, XxplanmaxCalender, XxplanmaxHeaderDtls.XxplanmaxCalender_day_id.expression)
+    __table__ = outerjoin(XxplanmaxHeaderDtls, XxplanmaxCalender, XxplanmaxHeaderDtls.XxplanmaxCalender_day_id.expression)
     __table_properties__ = {"ui_x_pos": 503.13734483912555, "ui_y_pos": 891.5731584714746, "colour": "#F2F3F5"}
 
 
